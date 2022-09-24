0088ffe8eb8bff37132ea0eacb824157ef8a1c65
Alexander Rolfes <9316893+Wiseqube@users.noreply.github.com>
2022-09-24 20:52:41 +0200

    First implementation of TItle search (#6)
    * First implementation of TItle search
    
    * Add id for input box and run prettier

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,27 @@
 import React from "react";
+import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const Banner = (props) => {
+  const handleSearchEntry = (ev) => {
+    ev.preventDefault();
+    let title = ev.target.value;
+    if (title.length >= 3) {
+      props.onSearchTitle(
+        title,
+        (page) => agent.Items.byTitle(title, page),
+        agent.Items.byTitle(title)
+      );
+    }
+  };
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input id="search-box" onChange={handleSearchEntry}></input>
           <span> the cool stuff.</span>
         </div>
       </div>
--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -10,6 +10,7 @@ import {
   PROFILE_PAGE_UNLOADED,
   PROFILE_FAVORITES_PAGE_LOADED,
   PROFILE_FAVORITES_PAGE_UNLOADED,
+  APPLY_TITLE_SEARCH,
 } from "../constants/actionTypes";
 
 const reducer = (state = {}, action) => {
@@ -44,6 +45,15 @@ const reducer = (state = {}, action) => {
         itemsCount: action.payload.itemsCount,
         tab: null,
         tag: action.tag,
+        currentPage: 0,
+      };
+    case APPLY_TITLE_SEARCH:
+      return {
+        ...state,
+        pager: action.pager,
+        items: action.payload.items,
+        itemsCount: action.payload.itemsCount,
+        tab: null,
         currentPage: 0,
       };
     case HOME_PAGE_LOADED:
