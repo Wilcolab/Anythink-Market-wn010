fc466da6f702e244768cdda19d0ee3100cc5eb15
Alexander Rolfes <9316893+Wiseqube@users.noreply.github.com>
2023-03-03 20:42:37 +0100

    Implement isVerified flag for users (#18)
    * Add migration to create new column
    
    * Read is_verified from database and provide it via items api

--- backend/app/models/domain/users.py
+++ backend/app/models/domain/users.py
@@ -15,6 +15,7 @@ class User(RWModel):
 class UserInDB(IDModelMixin, DateTimeModelMixin, User):
     salt: str = ""
     hashed_password: str = ""
+    is_verified: bool
 
     def check_password(self, password: str) -> bool:
         return security.verify_password(self.salt + password, self.hashed_password)
